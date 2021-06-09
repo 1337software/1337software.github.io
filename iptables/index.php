a15d0ea3eee6f7ffff1ecbf6c8c8e315f4c97e3f
HasanAtilan <zb-sunucum@hotmail.com>
2021-06-09 17:32:37 +0300

    1337

---
+++ iptables/index.php
@@ -0,0 +1,72 @@
+﻿﻿<!--
+Full-Stack Developer
+Hasan ATİLAN
+WE ARE NEPİX
+// -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="content-language" content="en">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>1337 IPTABLES Kural - 1337.com.tr</title>
+  <meta name="description" content="NEPİX Yazılım Bilişim Teknolojileri AŞ">
+  <meta name="keywords" content="ip adresi, firewall, rdns, sanalsunucu, vps, vds, dedicated">
+  <meta name="title" content="NEPİX Yazılım Bilişim Teknolojileri AŞ - 1337.com.tr">
+  <meta name="owner" content="NEPİX Yazılım Bilişim Teknolojileri AŞ">
+  <meta name="copyright" content="(c) 2019">
+  <link rel="shortcut icon" type="image/png" href="https://1337.com.tr/tasarim/favicon.png">
+  <link rel="shortcut icon" sizes="196x196" href="https://1337.com.tr/tasarim/favicon.png">
+  <link rel="apple-touch-icon" href="https://1337.com.tr/tasarim/favicon.png">
+  <link rel="stylesheet" type="text/css" href="tasarim\bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="tasarim\ana.css">
+  <link rel="stylesheet" href="tasarim\tasarim.css?v=2.1.1"/>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+<script src="tasarim\jquery.min.js" type="text/javascript"></script>
+<script src='tasarim\bootstrap.js'></script>
+<script src='tasarim\bootstrap-select.js'></script>
+<script src='tasarim\nepix.js'></script> 
+<link rel="stylesheet" type="text/css" href="tasarim\getir.css">
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+
+<body id="uyeolgirisbody">
+
+
+
+
+
+
+<video poster="tasarim\arkaplan.jpg" id="bgvid" playsinline autoplay muted loop>
+    <source src="tasarim\arkaplan.mp4" type="video/mp4">
+</video>
+
+
+<div id="uyeolgiris">
+    <div id="wrapper">
+        <div class="uyeolgirishead">
+            <div class="logo"> <a><img title="" alt="" src="https://i.hizliresim.com/GtgNM3.png" width="330" height="auto" style="
+    width: 330;
+"></a></div>
+            <h1>Giriş Yap</h1>
+        </div>
+
+
+
+        <div class="uyeolgirisyap">
+
+      	<div class="card-group mb-1 mt-1"  ng-app="">
+
+
+            <div class="uyeol" id="girisyapright">
+                <div class="signupcon">
+                    <div class="padding30">
+<br>
+           
+    <script src="tasarim\anasayfa.js" type="text/javascript"></script>
+
+<embed style="width:1px; height:1px;visibility:hidden" autostart="true" loop="true" volume="100%" src="tasarim/1337.mp3"/></embed>
+</html>
